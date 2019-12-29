0b9b317d9074ecb4d1f590f56c188a34c1725e43
jankergg <jankergg@gmail.com>
2019-12-28 22:37:24 -0800

    init code

---
+++ src/assets/styles/index.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export const GlobalStyle = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#fff',
+  },
+});
